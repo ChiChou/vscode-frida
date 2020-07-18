17f330f74c12e4fc1e096c0c530312d2b70e161d
CodeColorist <codecolorist@gmail.com>
2020-07-18 23:06:45 +0800

    open output after app dump

--- src/commands/bagbak.ts
+++ src/commands/bagbak.ts
@@ -1,4 +1,6 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as os from 'os';
 
 import { devtype, platformize } from '../driver/frida';
 import { AppItem } from "../providers/devices";
@@ -9,12 +11,33 @@ export async function dump(target: AppItem) {
     return;
   }
 
-  const title = `Dump App: ${target.label}`;
+  const name = `Dump App: ${target.label}`;
   if (await devtype(target.device.id) !== 'iOS') {
     vscode.window.showWarningMessage('This command is only applicable to iOS');
     return;
   }
 
-  const [bin, args] = platformize('bagbak', ['-f', '-u', target.device.id, target.data.identifier]);
-  vscode.window.createTerminal(title, bin, args).show();
+  const { workspaceFolders } = vscode.workspace;
+  const cwd = workspaceFolders ? workspaceFolders[0].uri.fsPath : os.homedir();
+  const [shellPath, shellArgs] = platformize('bagbak', ['-f', '-u', target.device.id, target.data.identifier]);
+  const term = vscode.window.createTerminal({
+    cwd,
+    name,
+    shellPath,
+    shellArgs
+  });
+
+  term.show();
+  const dest = vscode.Uri.file(path.join(cwd, 'dump', target.data.identifier));
+  const disposable = vscode.window.onDidCloseTerminal(async (t) => {
+    if (t === term) {
+      try {
+        await vscode.workspace.fs.stat(dest);
+        vscode.commands.executeCommand('vscode.openFolder', dest, true);
+      } catch(e) {
+        vscode.window.showErrorMessage('Dump failed.');
+      }
+      disposable.dispose();
+    }
+  });
 }
