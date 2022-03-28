f6d0424b247da59addeac90034172f7562a3b72b
1173372284@qq.com <1173372284@qq.com>
2022-03-29 05:42:51 +0800

    fixed ancondo env frida command not fond

--- backend/pause.py
+++ backend/pause.py
@@ -1,10 +1,23 @@
 import subprocess
 import sys
+import os
+
+def get_environ():
+  environ = os.environ
+  PATH = [environ['PATH']]
+  for p in sys.path:
+    if os.path.isdir(p):
+        PATH.append(p)
+  flag = ':'
+  if sys.platform == 'win32':
+    flag=';'
+  environ['PATH'] = flag.join(PATH)
 
 
 if len(sys.argv) > 1:
+  env = get_environ()
   try:
-    subprocess.check_call(sys.argv[1:])
+    subprocess.check_call(sys.argv[1:], env=env)
   except (subprocess.SubprocessError, FileNotFoundError) as e:
     print(e)
   finally:
