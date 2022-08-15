8f14006a21cce0628e708e998e3a482c19dfb33c
cc <codecolorist@gmail.com>
2022-08-16 00:18:01 +0200

    remove fs agent

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -1,11 +1,9 @@
-import * as fs from './fs.js';
 import { start, stop } from './log.js';
 import { copyid, signDebugserver } from './sshagent.js';
 
 const ping = () => Process.id;
 
 rpc.exports = {
-    fs: fs.invoke,
     start,
     stop,
     ping,
