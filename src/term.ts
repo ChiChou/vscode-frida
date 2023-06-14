79086765f530445131abdb3bc2911a1913c47555
cc <codecolorist@gmail.com>
2023-06-14 02:20:31 +0200

    add custom error for fruity-frida

--- src/term.ts
+++ src/term.ts
@@ -1,13 +1,22 @@
 import { TerminalOptions, window } from "vscode";
 
+export class CommandNotFoundError extends Error {
+  constructor(cmd: string) {
+    super(`Command not found: ${cmd}`);
+  }
+}
+
 export function run(opt: TerminalOptions) {
+  const ext: TerminalOptions = { hideFromUser: true };
   return new Promise<void>((resolve, reject) => {
-    const ext: TerminalOptions = { hideFromUser: true };
     const term = window.createTerminal(Object.assign(opt, ext));
 
     const disposable = window.onDidCloseTerminal(terminal => {
       if (terminal !== term) { return; }
-      if (term.exitStatus?.code !== 0) {
+
+      if (term.exitStatus?.code === undefined && opt.shellPath) {
+        reject(new CommandNotFoundError(opt.shellPath));
+      } else if (term.exitStatus?.code !== 0) {
         reject(new Error(`Terminal exited with status ${term.exitStatus?.code}`));
       } else {
         resolve();
