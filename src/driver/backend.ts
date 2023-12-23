b4d72dddf9a2f096f1122bb6760095eb8f1317fa
cc <codecolorist@gmail.com>
2023-12-23 23:56:58 +0100

    delete unused code

--- src/driver/backend.ts
+++ src/driver/backend.ts
@@ -94,15 +94,3 @@ export function lockdownSyslog(id: string, bundleOrPid: string[]) {
     shellArgs: [py, 'syslog2', '--device', id, ...bundleOrPid]
   });
 }
-
-export function path(...args: string[]) {
-  return join(base, ...args);
-}
-
-export function fruit(...args: string[]) {
-  return path('fruit', ...args);
-}
-
-export function android(...args: string[]) {
-  return path('android', ...args);
-}
