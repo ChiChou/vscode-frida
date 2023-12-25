62d0e22db042a2c2e2bd7a1b0851f6b058c2bae4
cc <codecolorist@gmail.com>
2023-12-25 19:48:11 +0100

    copy hello world sample from vscode-webview-ui-toolkit

---
+++ webview-ui/src/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { provideVSCodeDesignSystem, vsCodeButton } from "@vscode/webview-ui-toolkit";
+import { vscode } from "./utilities/vscode";
+
+// In order to use the Webview UI Toolkit web components they
+// must be registered with the browser (i.e. webview) using the
+// syntax below.
+provideVSCodeDesignSystem().register(vsCodeButton());
+
+// To register more toolkit components, simply import the component
+// registration function and call it from within the register
+// function, like so:
+//
+// provideVSCodeDesignSystem().register(
+//   vsCodeButton(),
+//   vsCodeCheckbox()
+// );
+//
+// Finally, if you would like to register all of the toolkit
+// components at once, there's a handy convenience function:
+//
+// provideVSCodeDesignSystem().register(allComponents);
+
+function handleHowdyClick() {
+  vscode.postMessage({
+    command: "hello",
+    text: "Hey there partner! 🤠",
+  });
+}
+</script>
+
+<template>
+  <main>
+    <h1>Hello world!</h1>
+    <vscode-button @click="handleHowdyClick">Howdy!</vscode-button>
+  </main>
+</template>
+
+<style>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  height: 100%;
+}
+</style>
---
+++ webview-ui/vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+})
