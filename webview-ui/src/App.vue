9666e888c971cdc11a34f8dcef026a0e69f7dbd8
cc <codecolorist@gmail.com>
2023-12-26 10:53:19 +0100

    import vue-router

--- webview-ui/src/App.vue
+++ webview-ui/src/App.vue
@@ -1,38 +1,6 @@
-<script setup lang="ts">
-import { provideVSCodeDesignSystem, vsCodeButton } from "@vscode/webview-ui-toolkit";
-import { vscode } from "./utilities/vscode";
-
-// In order to use the Webview UI Toolkit web components they
-// must be registered with the browser (i.e. webview) using the
-// syntax below.
-provideVSCodeDesignSystem().register(vsCodeButton());
-
-// To register more toolkit components, simply import the component
-// registration function and call it from within the register
-// function, like so:
-//
-// provideVSCodeDesignSystem().register(
-//   vsCodeButton(),
-//   vsCodeCheckbox()
-// );
-//
-// Finally, if you would like to register all of the toolkit
-// components at once, there's a handy convenience function:
-//
-// provideVSCodeDesignSystem().register(allComponents);
-
-function handleHowdyClick() {
-  vscode.postMessage({
-    command: "hello",
-    text: "Hey there partner! 🤠",
-  });
-}
-</script>
-
 <template>
   <main>
-    <h1>Hello world!</h1>
-    <vscode-button @click="handleHowdyClick">Howdy!</vscode-button>
+    <RouterView></RouterView>
   </main>
 </template>
 
--- webview-ui/src/main.ts
+++ webview-ui/src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
+
+import router from "./router";
+
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+createApp(App).use(router).mount("#app");
