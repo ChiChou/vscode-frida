0f5a5a42f1ebba8007eb6c33318c17ea09150301
cc <codecolorist@gmail.com>
2024-10-28 00:58:27 +0100

    use early return

--- src/commands/objection.ts
+++ src/commands/objection.ts
@@ -6,48 +6,51 @@ import { run } from '../term';
 import { DeviceType } from '../types';
 import { python3Path } from '../utils';
 
-export async function explore(target: TargetItem) {
+export async function explore(target: TargetItem) : Promise<void> {
   if (!target) {
     vscode.window.showErrorMessage('This command is only expected to be used in the context menu');
     return;
   }
 
+  if (!(target instanceof AppItem || target instanceof ProcessItem)) {
+    vscode.window.showErrorMessage('This command is not applicable to the selected item');
+    return;
+  }
+
   const name = `Objection - ${target.label}`;
 
-  if (target instanceof AppItem || target instanceof ProcessItem) {
-    let device: string[];
-    switch (target.device.type) {
-      case DeviceType.TCP:
-      case DeviceType.Remote:
-        // todo: support remote connection
-        device = ['-N', '-h', target.device.id];
-        break;
-      case DeviceType.Local:
-        device = [];
-        vscode.window.showErrorMessage('This command is not applicable to the local device');
-        return;
-      case DeviceType.USB:
-      default:
-        device = [];
-    }
-
-    let { pid } = target.data;
-    let gadget = pid.toString();
-    if (target instanceof AppItem && !pid) {
-      try {
-        gadget = (await launch(target.device.id, target.data.identifier)).toString();
-      } catch (e) {
-        vscode.window.showWarningMessage(`Warning: failed to launch App ${target.data.identifier}\n${e}`);
-        gadget = target.data.name;
-      }
-    }
-
-    const shellArgs = ['-m', 'objection.console.cli', '-g', gadget, ...device, 'explore'];
-    const shellPath = python3Path();
-    run({
-      name,
-      shellArgs,
-      shellPath,
-    });
+  let device: string[];
+  switch (target.device.type) {
+    case DeviceType.TCP:
+    case DeviceType.Remote:
+      // todo: support remote connection
+      device = ['-N', '-h', target.device.id];
+      break;
+    case DeviceType.Local:
+      device = [];
+      vscode.window.showErrorMessage('This command is not applicable to the local device');
+      return;
+    case DeviceType.USB:
+    default:
+      device = [];
   }
+
+  let { pid } = target.data;
+  let gadget = pid.toString();
+  if (target instanceof AppItem && !pid) {
+    try {
+      gadget = (await launch(target.device.id, target.data.identifier)).toString();
+    } catch (e) {
+      vscode.window.showWarningMessage(`Warning: failed to launch App ${target.data.identifier}\n${e}`);
+      gadget = target.data.name;
+    }
+  }
+
+  const shellArgs = ['-m', 'objection.console.cli', '-g', gadget, ...device, 'explore'];
+  const shellPath = python3Path();
+  run({
+    name,
+    shellArgs,
+    shellPath,
+  });
 }
