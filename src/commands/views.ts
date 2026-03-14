d61cf24ff24bb6981ed3f5d9d00cba78667eef47
cc <codecolorist@gmail.com>
2026-03-14 02:07:27 +0100

    bugfix: add null check

--- src/commands/views.ts
+++ src/commands/views.ts
@@ -11,42 +11,49 @@ export function init(context: vscode.ExtensionContext) {
 }
 
 export async function modules(target: TargetItem) {
+  if (!target) { return; }
   logger.appendLine(`Open modules panel for ${target.label}`);
   const { ModulesPanel } = await import('../webview/ModulesPanel');
   new ModulesPanel(extensionUri, target).show();
 }
 
 export async function classes(target: TargetItem) {
+  if (!target) { return; }
   logger.appendLine(`Open classes panel for ${target.label}`);
   const { ClassesPanel } = await import('../webview/ClassesPanel');
   new ClassesPanel(extensionUri, target).show();
 }
 
 export async function protocols(target: TargetItem) {
+  if (!target) { return; }
   logger.appendLine(`Open protocols panel for ${target.label}`);
   const { ProtocolsPanel } = await import('../webview/ProtocolsPanel');
   new ProtocolsPanel(extensionUri, target).show();
 }
 
 export async function detail(device: DeviceItem) {
+  if (!device) { return; }
   logger.appendLine(`Open device detail for ${device.data.name}`);
   const { DeviceDashboardPanel } = await import('../webview/DeviceDashboardPanel');
   new DeviceDashboardPanel(extensionUri, device).show();
 }
 
 export async function memory(target: TargetItem) {
+  if (!target) { return; }
   logger.appendLine(`Open memory panel for ${target.label}`);
   const { MemoryPanel } = await import('../webview/MemoryPanel');
   new MemoryPanel(extensionUri, target).show();
 }
 
 export async function scanner(target: TargetItem) {
+  if (!target) { return; }
   logger.appendLine(`Open memory scanner for ${target.label}`);
   const { MemoryScannerPanel } = await import('../webview/MemoryScannerPanel');
   new MemoryScannerPanel(extensionUri, target).show();
 }
 
 async function fetchAndShow(target: TargetItem, method: string, filename: string) {
+  if (!target) { return; }
   logger.appendLine(`Fetching ${method} for ${target.label}`);
   await vscode.window.withProgress(
     { location: vscode.ProgressLocation.Notification, title: vscode.l10n.t('Loading...') },
